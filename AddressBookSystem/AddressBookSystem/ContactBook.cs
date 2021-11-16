df588c5bddeaf210b0bbe36adedaca14c03403de
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-16 19:26:14 +0530

    [gunasekhar9] add : Adding Contacts is Done

--- AddressBookSystem/AddressBookSystem/ContactBook.cs
+++ AddressBookSystem/AddressBookSystem/ContactBook.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace AddressBookSystem
 {
-    public class ContactBook
+    public class Contact
     {
-        List<ContactBook> addressList = new List<ContactBook>();
+        List<Contact> addressList = new List<Contact>();
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
@@ -15,101 +15,5 @@ namespace AddressBookSystem
         public string Zip { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
-        public void AddContact()
-        {
-            ContactBook contact = new ContactBook();
-            Console.Write("Enter First Name : ");
-            contact.FirstName = Console.ReadLine();
-            Console.Write("Enter Last Name : ");
-            contact.LastName = Console.ReadLine();
-            Console.Write("Enter Address : ");
-            contact.Address = Console.ReadLine();
-            Console.Write("Enter City : ");
-            contact.City = Console.ReadLine();
-            Console.Write("Enter State : ");
-            contact.State = Console.ReadLine();
-            Console.Write("Enter the Zip code : ");
-            contact.Zip = Console.ReadLine();
-            Console.Write("Enter Phone Number : ");
-            contact.PhoneNumber = Console.ReadLine();
-            Console.Write("Enter Email ID : ");
-            contact.Email = Console.ReadLine();
-            addressList.Add(contact);
-        }
-        public void EditContact(string name)
-        {
-            foreach (var contact in addressList)
-            {
-                if (contact.FirstName == name || contact.LastName == name)
-                {
-                    Console.WriteLine("Choose the field you want to edit : \n 1. First name \n 2. Last name \n 3. Address \n 4. City \n 5. State \n 6. Zip code \n 7. Phone Number \n 8. Email");
-                    int choice = Convert.ToInt32(Console.ReadLine());
-                    switch (choice)
-                    {
-                        case 1:
-                            Console.WriteLine("Enter you want to edit");
-                            string first = Console.ReadLine();
-                            contact.FirstName = first;
-                            break;
-                        case 2:
-                            Console.WriteLine("Enter you want to edit");
-                            string last = Console.ReadLine();
-                            contact.LastName = last;
-                            break;
-                        case 3:
-                            Console.WriteLine("Enter you want to edit");
-                            string address = Console.ReadLine();
-                            contact.Address = address;
-                            break;
-                        case 4:
-                            Console.WriteLine("Enter you want to edit");
-                            string city = Console.ReadLine();
-                            contact.City = city;
-                            break;
-                        case 5:
-                            Console.WriteLine("Enter you want to edit");
-                            string state = Console.ReadLine();
-                            contact.State = state;
-                            break;
-                        case 6:
-                            Console.WriteLine("Enter you want to edit");
-                            string zip = Console.ReadLine();
-                            contact.Zip = zip;
-                            break;
-                        case 7:
-                            Console.WriteLine("Enter you want to edit");
-                            string phone = Console.ReadLine();
-                            contact.PhoneNumber = phone;
-                            break;
-                        case 8:
-                            Console.WriteLine("Enter you want to edit");
-                            string email = Console.ReadLine();
-                            contact.Email = email;
-                            break;
-                    }
-                }
-            }
-        }
-        public void DeleteContact(string name)
-        {
-            ContactBook delete = new ContactBook();
-            foreach (var contact in addressList)
-            {
-                if (contact.FirstName == name || contact.LastName == name)
-                {
-                    delete = contact;
-                }
-            }
-            addressList.Remove(delete);
-            Console.WriteLine(name + " contact is deleted from the Address Book");
-        }
-        public void Display()
-        {
-            foreach (var contact in addressList)
-            {
-                Console.WriteLine("********************************");
-                Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
-            }
-        }
     }
 }
--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -9,14 +9,14 @@ namespace AddressBookSystem
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Welcome to Address Book Program");
-                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Add Contacts \n 3. Edit Contact \n 4. Delete Contact \n 5. Exit");
+                Console.WriteLine("Welcome to the Address Book Program");
+                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Add Contacts \n 3. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Ph.no, Email");
-                        ContactBook contact = new ContactBook()
+                        Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Ph.no, Email : ");
+                        Contact contact = new Contact()
                         {
                             FirstName = Console.ReadLine(),
                             LastName = Console.ReadLine(),
@@ -29,28 +29,24 @@ namespace AddressBookSystem
                         };
                         break;
                     case 2:
-                        Console.WriteLine("Enter the contact to be added : ");
-                        ContactBook con = new ContactBook();
-                        con.AddContact();
-                        con.Display();
+                        Console.WriteLine("Enter the Contact Information to Add in phoneBook in form of FirstName, LastName, Addr, City, State, Zip, Ph.No, Email.");
+                        Console.WriteLine("Enter the Contact to Add: ");
+                        AddressBook addressBook = new AddressBook();
+                        Contact cont = new Contact()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            PhoneNumber = Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
+                        addressBook.AddContact(cont);
+                        addressBook.Display();
                         break;
                     case 3:
-                        Console.WriteLine("Enter the contact which you want to Edit");
-                        string name = Console.ReadLine();
-                        ContactBook edit = new ContactBook();
-                        edit.AddContact();
-                        edit.EditContact(name);
-                        edit.Display();
-                        break;
-                    case 4:
-                        Console.WriteLine("Enter the contact you want to delete :");
-                        string detail = Console.ReadLine();
-                        ContactBook delete = new ContactBook();
-                        delete.AddContact();
-                        delete.DeleteContact(detail);
-                        delete.Display();
-                        break;
-                    case 5:
                         flag = false;
                         break;
                 }
