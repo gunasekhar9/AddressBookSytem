55150e7fb30cb604bca6060dee3330bf2db1252f
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-17 18:29:12 +0530

    [gunasekhar9] add : Edit Contacts is Done

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -7,16 +7,18 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             bool flag = true;
+            Contact contact = new Contact();
+            AddressBook addressBook = new AddressBook();
             while (flag)
             {
                 Console.WriteLine("Welcome to the Address Book Program");
-                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Add Contacts \n 3. Exit");
+                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Add Contacts \n 3. Edit Contacts \n 4. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Ph.no, Email : ");
-                        Contact contact = new Contact()
+                        contact = new Contact()
                         {
                             FirstName = Console.ReadLine(),
                             LastName = Console.ReadLine(),
@@ -29,10 +31,8 @@ namespace AddressBookSystem
                         };
                         break;
                     case 2:
-                        Console.WriteLine("Enter the Contact Information to Add in phoneBook in form of FirstName, LastName, Addr, City, State, Zip, Ph.No, Email.");
-                        Console.WriteLine("Enter the Contact to Add: ");
-                        AddressBook addressBook = new AddressBook();
-                        Contact cont = new Contact()
+                        Console.WriteLine("Enter the Contact Information to Add, in form of FirstName, LastName, Addr, City, State, Zip, Ph.No, Email.");
+                        contact = new Contact()
                         {
                             FirstName = Console.ReadLine(),
                             LastName = Console.ReadLine(),
@@ -43,10 +43,17 @@ namespace AddressBookSystem
                             PhoneNumber = Console.ReadLine(),
                             Email = Console.ReadLine(),
                         };
-                        addressBook.AddContact(cont);
+                        addressBook.AddContact(contact);
                         addressBook.Display();
                         break;
                     case 3:
+                        Console.WriteLine("Enter the Contact Name to  Edit: ");
+                        string name = Console.ReadLine();
+                        addressBook.EditContact(name);
+                        Console.WriteLine("Contact is Edited Sucsessfully");
+                        addressBook.Display();
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
