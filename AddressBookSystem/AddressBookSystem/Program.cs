595ea35a88e064ca2aa961740180462792ed66e0
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-12 14:42:05 +0530

    [gunasekhar9] add : Sorting Based on City,State, or Zip

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -12,7 +12,7 @@ namespace AddressBookSystem
             while (flag)
             {
                 Console.WriteLine("Welcome to the Address Book Program");
-                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Add Contacts \n 3. Edit Contacts \n 4. Delete Contact \n 5. Add Multiple Contact \n 6. Add Unique Contact \n 7. Check For Duplicate Entry \n 8. Check for Person in City or State \n 9. View Persons by City or State \n 10. Number of persons by City or State \n 11. AddressBookSorting \n 12.Exit");
+                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Add Contacts \n 3. Edit Contacts \n 4. Delete Contact \n 5. Add Multiple Contact \n 6. Add Unique Contact \n 7. Check For Duplicate Entry \n 8. Check for Person in City or State \n 9. View Persons by City or State \n 10. Number of persons by City or State \n 11. AddressBookSorting \n 12. Sorting entries by City, State or Zipcode \n 13. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -115,6 +115,9 @@ namespace AddressBookSystem
                         addressBook.AddressBookSorting();
                         break;
                     case 12:
+                        addressBook.Sorting();
+                        break;
+                    case 13:
                         flag = false;
                         break;
                 }
